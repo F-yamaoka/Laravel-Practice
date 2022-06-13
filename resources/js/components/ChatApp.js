1c00653e8398b89fce474056cb6993663bd01d32
PPF\yamaoka <104489457+F-yamaoka@users.noreply.github.com>
2022-06-13 14:51:22 +0900

    名前のヴァリデーションがpromptで開いてしまっていたのをalertに変更

--- resources/js/components/ChatApp.js
+++ resources/js/components/ChatApp.js
@@ -29,8 +29,8 @@ export default class ChatApp extends Component {
     while (1){
       input_name = prompt('名前を入力','');
       if (input_name == null) return;
-      if (input_name == 'noname') prompt ('この名前は使用できません');
-      else if (input_name.length > 20) prompt('20文字以下で入力してください');
+      if (input_name == 'noname') alert('この名前は使用できません');
+      else if (input_name.length > 20) alert('20文字以下で入力してください');
       else  break;
     }
     this.setState((state)=>({
